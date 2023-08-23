c4a25cdd7d2c8d92c96928990999c5049401669e
Manav Gondalia <gondaliamanav@gmail.com>
2023-08-24 00:02:05 +0530

    Update GUI and Add more sorting algorithms

--- sorted.py
+++ sorted.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import random
 import pygame
 import pygame_menu
 
 pygame.init()
 surface = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("Sorter")
-LABEL = "Welcome to Sorter! \n Press any button to see the sorting algorithm in action. \n Or navigate using the arrow keys."
+LABEL = "Welcome to Sorter! \nNavigate using the arrow keys or mouse.\n Choose array elements randomly or by yourself."
 
 img = pygame.image.load('icon.png')
 pygame.display.set_icon(img)
@@ -15,6 +15,7 @@ pygame.display.set_icon(img)
 
 x = []
 y = []
+y_custom = np.array([])
 SORTED = "#EB455F"
 NOTSORTED = "#2B3467"
 SELECTED = "#F7B801"
@@ -23,6 +24,7 @@ SELECTED = "#F7B801"
 n = 20
 y = np.random.randint(0, 250, n)
 x = np.arange(0, n, 1)
+use_custom_array = False
 
 
 def bubble_sort(xcopy, ycopy, ncopy):
@@ -158,11 +160,106 @@ def merge_sort(xcopy, ycopy, l, r, ncopy, color):
         return
 
 
+def partition(xcopy, ycopy, l, r, ncopy, color):
+    pivot = ycopy[r]
+    color[r] = SELECTED
+    i = l-1
+    for j in range(l, r):
+        if(ycopy[j] < pivot):
+            i += 1
+            ycopy[i], ycopy[j] = ycopy[j], ycopy[i]
+
+            plt.bar(xcopy, ycopy, color=color)
+            plt.pause(n/1000)
+            plt.clf()
+
+    ycopy[i+1], ycopy[r] = ycopy[r], ycopy[i+1]
+    color[i+1] = SORTED
+    plt.bar(xcopy, ycopy, color=color)
+    plt.pause(n/1000)
+    plt.clf()
+    return i+1
+
+
+def quick_sort(xcopy, ycopy, l, r, ncopy, color):
+    if(l <= r):
+        pi = partition(xcopy, ycopy, l, r, ncopy, color)
+
+        plt.bar(xcopy, ycopy, color=color)
+        plt.pause(n/1000)
+        plt.clf()
+
+        quick_sort(xcopy, ycopy, l, pi-1, ncopy, color)
+        quick_sort(xcopy, ycopy, pi+1, r, ncopy, color)
+
+        plt.bar(xcopy, ycopy, color=color)
+        plt.pause(n/1000)
+        plt.clf()
+
+
 def set_insanity(value, size):
     global n, x, y
     n = size
     y = np.random.randint(0, 250, n)
     x = np.arange(0, n, 1)
+    print(f"Will use the random array: {y}")
+
+
+def generate_random_array():
+    global n, x, y
+    y = np.random.randint(0, 250, n)
+    x = np.arange(0, n, 1)
+    print(f"Will use the random array: {y}")
+
+
+def set_algo(value, algo):
+    global selected_algorithm
+    selected_algorithm = value
+
+
+def start_algorithm():
+    print("#"*50)
+    print(f"Running {selected_algorithm[0][0]} ...")
+    selected_algorithm[0][1]()
+    print(f"{selected_algorithm[0][0]} completed.")
+    print("#"*50)
+
+
+def set_array(value):
+    global n, x, y, y_custom
+    y_custom = np.array(value.split(' '))
+    n_custom = len(y)
+    x_custom = np.arange(0, n, 1)
+    if(use_custom_array):
+        y = y_custom
+        n = len(y)
+        x = np.arange(0, n, 1)
+        try:
+            y = y.astype(int)
+            print(f"Will be using the custom array: {y_custom}")
+        except:
+            print(
+                "Please enter valid integers separated by spaces or don't use custom input.")
+
+
+def toggle_custom_array(value):
+    global use_custom_array, y_custom, n, x, y
+    use_custom_array = value
+    if(use_custom_array):
+        print(f"Will be using the custom array: {y_custom}")
+        y = y_custom
+        n = len(y)
+        x = np.arange(0, n, 1)
+        try:
+            y = y.astype(int)
+        except:
+            print(
+                "Please enter valid integers separated by spaces or don't use custom input.")
+    else:
+        n = 20
+        y = np.random.randint(0, 250, n)
+        x = np.arange(0, n, 1)
+        print(f"Will use the random array: {y}")
 
 
 def bt():
@@ -190,22 +287,22 @@ def mt():
     plt.show()
 
 
-def set_algo(value, algo):
-    global selected_algorithm
-    selected_algorithm = value
+def qt():
+    color = []
+    for i in range(n):
+        color.append(NOTSORTED)
+    xcopy = np.copy(x)
+    ycopy = np.copy(y)
+    ncopy = n
+    quick_sort(xcopy, ycopy, 0, ncopy-1, ncopy, color)
+    plt.title("Quick Sort Completed")
+    plt.bar(xcopy, ycopy, color=color)
+    plt.show()
 
-
-def start_algorithm():
-    selected_algorithm[0][1]()
-
-def set_array(value):
-    global n, x, y
-    y = np.array(value.split(','))
-    n = len(y)
-    x = np.arange(0, n, 1)
 
 algorithms = [("Bubble Sort", bt), ("Selection Sort", st),
-              ("Insertion Sort", it), ("Merge Sort", mt)]
+              ("Insertion Sort", it), ("Merge Sort", mt), ("Quick Sort", qt)]
+
 selected_algorithm = (("Bubble Sort", bt), 0)
 
 
@@ -213,21 +310,23 @@ selected_algorithm = (("Bubble Sort", bt), 0)
 menu = pygame_menu.Menu('Welcome', 1000, 600,
                         theme=pygame_menu.themes.THEME_DARK)
 
-menu.add.label(LABEL)
+menu.add.label(LABEL, max_char=-1, font_size=25, font_color=(255, 255, 255))
 
-menu.add.selector(
-    'Size of array :', [('Mild: 20', 20), ('Bold: 50', 50), ('Insane: 100', 100)], onchange=set_insanity)
+menu.add.dropselect(
+    'Size of array :', [('Small: 20', 20), ('Medium: 50', 50), ('Large: 100', 100)], onchange=set_insanity)
 
+menu.add.button("New random array", generate_random_array)
+
+
+menu.add.toggle_switch("Use custom array: ", False,
+                       onchange=toggle_custom_array)
+menu.add.text_input("Enter array elements: ",
+                    input_underline='_', onchange=set_array)
 select_algorithm = menu.add.dropselect(
     'Algorithm to run:', algorithms, default=0, onchange=set_algo)
+menu.add.button('Start', start_algorithm,
+                align=pygame_menu.locals.ALIGN_CENTER)
+menu.add.button('Quit', pygame_menu.events.EXIT,
+                align=pygame_menu.locals.ALIGN_CENTER)
 
-menu.add.button('Start', start_algorithm)
-
-menu.add.text_input("Enter array elements: ", default='1,2,3,4,5,6,7,8,9,10', onchange=set_array)
-
-# menu.add.button('Bubble Sort', bt)
-# menu.add.button('Selection Sort', st)
-# menu.add.button('Insertion Sort', it)
-# menu.add.button('Merge Sort', mt)
-menu.add.button('Quit', pygame_menu.events.EXIT)
 menu.mainloop(surface)
